92904f5c043c451badbb4dc93a1f29e3f15f9a04
Sanjay Ramassery Babu <sanjaybabu618@gmail.com>
2023-08-05 09:57:05 +0530

    Added custom gif loader

--- lib/constants/assets.dart
+++ lib/constants/assets.dart
@@ -1,6 +1,30 @@
 class Assets {
   Assets._();
   
+  /// Assets for assetsImagesLoader0
+  /// assets/images/loader_0.png
+  static const String assetsImagesLoader0 = "assets/images/loader_0.png";
+
+  /// Assets for assetsImagesLoader1
+  /// assets/images/loader_1.png
+  static const String assetsImagesLoader1 = "assets/images/loader_1.png";
+
+  /// Assets for assetsImagesLoader2
+  /// assets/images/loader_2.png
+  static const String assetsImagesLoader2 = "assets/images/loader_2.png";
+
+  /// Assets for assetsImagesLoader3
+  /// assets/images/Loader_3.png
+  static const String assetsImagesLoader3 = "assets/images/Loader_3.png";
+
+  /// Assets for assetsImagesLoader4
+  /// assets/images/loader_4.png
+  static const String assetsImagesLoader4 = "assets/images/loader_4.png";
+
+  /// Assets for assetsImagesLoader5
+  /// assets/images/loader_5.png
+  static const String assetsImagesLoader5 = "assets/images/loader_5.png";
+
   /// Assets for assetsImagesRefreshLoader
   /// assets/images/refresh_loader.gif
   static const String assetsImagesRefreshLoader = "assets/images/refresh_loader.gif";
--- lib/constants/loader_widget.dart
+++ lib/constants/loader_widget.dart
@@ -1,8 +1,59 @@
 import 'package:flutter/material.dart';
 import 'package:mtracker/constants/assets.dart';
 
-class LoaderWidget extends StatelessWidget {
+class LoaderWidget extends StatefulWidget {
   const LoaderWidget({super.key});
+
+  @override
+  State<LoaderWidget> createState() => _LoaderWidgetState();
+}
+
+class _LoaderWidgetState extends State<LoaderWidget>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<int> _animation;
+  List<Image> images = [
+    Assets.assetsImagesLoader0,
+    Assets.assetsImagesLoader1,
+    Assets.assetsImagesLoader2,
+    Assets.assetsImagesLoader3,
+    Assets.assetsImagesLoader4,
+    Assets.assetsImagesLoader5,
+  ]
+      .map(
+        (e) => Image.asset(
+          e,
+          gaplessPlayback: true,
+          fit: BoxFit.cover,
+          cacheHeight: 1,
+          cacheWidth: 1,
+        ),
+      )
+      .toList();
+  @override
+  void initState() {
+    super.initState();
+
+    _controller = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 200),
+    )..repeat();
+    _animation = IntTween(begin: 0, end: 5).animate(_controller);
+  }
+
+  @override
+  void didChangeDependencies() {
+    for (var element in images) {
+      precacheImage(element.image, context);
+    }
+    super.didChangeDependencies();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -14,11 +65,11 @@ class LoaderWidget extends StatelessWidget {
         child: SizedBox(
           width: MediaQuery.of(context).size.width * 0.2,
           height: MediaQuery.of(context).size.width * 0.2,
-          child: Image.asset(
-            Assets.assetsImagesRefreshLoader,
-            fit: BoxFit.cover,
-            cacheHeight: 1,
-            cacheWidth: 1,
+          child: AnimatedBuilder(
+            animation: _animation,
+            builder: (context, child) {
+              return images[_animation.value];
+            },
           ),
         ),
       ),

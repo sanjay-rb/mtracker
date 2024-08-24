8194e3eef2e4d098c54083917bb55f6c754455aa
sanjay-rb <sanjaybabu618@gmail.com>
2024-08-24 08:01:54 +0530

    Create new project for release 2.0

--- deploy.py
+++
@@ -1,7 +0,0 @@
-import subprocess
-
-cmds = ["fvm flutter clean", "fvm flutter build web", "firebase deploy"]
-for cmd in cmds:
-    print(f"\n\n\n>>> {cmd}")
-    subprocess.run(cmd, shell=True)
-print(f"\n\n\n>>> MTrakcer New Version Got Deployed Successfully")
--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:mtracker/routes/route.dart';
-import 'package:mtracker/services/gsheet_service.dart';
-import 'package:url_strategy/url_strategy.dart';
-import 'package:google_fonts/google_fonts.dart';
 
-Future<void> main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  setPathUrlStrategy();
-  await GSheetService.init();
+void main() {
   runApp(const MyApp());
 }
 
@@ -18,13 +11,59 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Money Tracker',
-      debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        primarySwatch: Colors.deepPurple,
-        textTheme: GoogleFonts.ubuntuTextTheme(Theme.of(context).textTheme),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
       ),
-      initialRoute: MTrackerRoutes.home,
-      onGenerateRoute: MTrackerRoutes.onGenerateRoute,
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ),
     );
   }
 }
